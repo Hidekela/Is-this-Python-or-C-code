7943aab9fbac5e0219697b94684d1382c160ba3b
Hidekela <rhidekela@gmail.com>
2021-12-17 16:44:45 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Which Language?
+
+Which of these two codes do you think will run? main.py or main.c?
+
+*For fun* :satisfied:
---
+++ main.py
@@ -0,0 +1,4 @@
+#include <stdio.h> //Is this Python or C code?
+#define print(string) int main(int argc, char *argv[]) { printf(string); return 0;}
+
+print("Hello world!")
